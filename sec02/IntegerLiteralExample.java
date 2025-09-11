fd6f0719bc7dc819fcc94b4830b9487d165d418b
kimuni01 <minhowin1@naver.com>
2025-09-11 19:31:48 +0900

    Add files via upload

---
+++ sec02/IntegerLiteralExample.java
@@ -0,0 +1,15 @@
+package ch02.sec02;
+
+public class IntegerLiteralExample {
+    public static void main(String[] args) {
+        int var1 = 0b1011; // binary
+        int var2 = 0206; // octal
+        int var3 = 365; // decimal
+        int var4 = 0xB3; // hexadecimal
+
+        System.out.println("var1 : " + var1);
+        System.out.println("var2 : " + var2);
+        System.out.println("var3 : " + var3);
+        System.out.println("var4 : " + var4);
+    }
+}
