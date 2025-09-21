40c6e4e36c540fd267fea664f6fe4abc045f2322
kimuni01 <minhowin1@naver.com>
2025-09-21 15:09:03 +0900

    Add files via upload

---
+++ ch04/sec02/IfElseExample.java
@@ -0,0 +1,16 @@
+package ch04.sec02;
+
+public class IfElseExample {
+    public static void main(String[] args) {
+        int score = 85;
+
+        if (score >= 90) {
+            System.out.println("점수가 90 이상입니다.");
+            System.out.println("등급은 A입니다.");
+        }
+        else { // when score < 90
+            System.out.println("점수가 90 미만입니다.");
+            System.out.println("등급은 B입니다.");
+        }
+    }
+}
