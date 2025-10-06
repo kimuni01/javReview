cc644e2b55cf90440c356f12bab4c21931181283
kimuni01 <minhowin1@naver.com>
2025-10-06 11:49:33 +0900

    Add files via upload

---
+++ ch06/sec06/exam01/Car.java
@@ -0,0 +1,9 @@
+package ch06.sec06.exam01;
+
+public class Car {
+    // declaring a field
+    // field is a variable inside a class declaration
+    String model;
+    boolean start;
+    int speed;
+}
