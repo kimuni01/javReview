18216fdf8819d81324f6ad924b90ec307a2316f6
kimuni01 <minhowin1@naver.com>
2025-10-07 17:40:27 +0900

    Add files via upload

---
+++ ch06/sec06/exam02/Car.java
@@ -0,0 +1,11 @@
+package ch06.sec06.exam02;
+
+public class Car {
+    // declaring a field
+    // field is a variable inside a class declaration
+    String company = "현대자동차";
+    String model = "그랜져";
+    String color = "검정";
+    int maxSpeed = 350;
+    int speed;
+}
