7aa8ee8a75cc53032f8d50ef135a7a706225c73a
kimuni01 <minhowin1@naver.com>
2025-10-11 19:54:05 +0900

    Add files via upload

---
+++ ch06/sec08/exam04/Calculator.java
@@ -0,0 +1,13 @@
+package ch06.sec08.exam04;
+
+public class Calculator {
+    // area of a square
+    double areaRectangle(double width) {
+        return width * width;
+    }
+
+    // area of a rectangle
+    double areaRectangle(double width, double height) {
+        return width * height;
+    }
+}
