44ab3d52fdf11f9e01faf609972820a57e2c62d3
kimuni01 <minhowin1@naver.com>
2025-10-22 19:58:26 +0900

    Add files via upload

---
+++ ch07/sec06/package2/C.java
@@ -0,0 +1,12 @@
+package ch07.sec06.package2; // different package
+
+import ch07.sec06.package1.A;
+
+public class C {
+    // declaring method
+    public void method() {
+        // A a = new A(); // impossible
+        // a.field = "value"; // impossible
+        // a.method(); // impossible
+    }
+}
