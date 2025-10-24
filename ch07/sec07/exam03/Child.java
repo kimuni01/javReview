ce160eda2bf35a73054a68fac2b15f4b56995d54
kimuni01 <minhowin1@naver.com>
2025-10-24 22:39:59 +0900

    Add files via upload

---
+++ ch07/sec07/exam03/Child.java
@@ -0,0 +1,11 @@
+package ch07.sec07.exam03;
+
+public class Child extends Parent {
+    // declaring field
+    public String field2;
+
+    // declaring method
+    public void method3() {
+        System.out.println("Child-method3()");
+    }
+}
