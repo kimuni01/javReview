b325e52f75eb0574a3844ccc989e44ebcb1ec345
kimuni01 <minhowin1@naver.com>
2025-10-24 22:39:45 +0900

    Add files via upload

---
+++ ch07/sec07/exam03/Parent.java
@@ -0,0 +1,16 @@
+package ch07.sec07.exam03;
+
+public class Parent {
+    // declaring field
+    public String field1;
+
+    // declaring method
+    public void method1() {
+        System.out.println("Parent-method1()");
+    }
+
+    // declaring method
+    public void method2() {
+        System.out.println("Parent-method2()");
+    }
+}
