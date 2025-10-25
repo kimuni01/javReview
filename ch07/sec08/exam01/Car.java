70f87c59b151a1a549bf526330ab3fda738564d0
kimuni01 <minhowin1@naver.com>
2025-10-25 19:33:25 +0900

    Add files via upload

---
+++ ch07/sec08/exam01/Car.java
@@ -0,0 +1,12 @@
+package ch07.sec08.exam01;
+
+public class Car {
+    // declaring field
+    public Tire tire;
+
+    // declaring method
+    public void run() {
+        // calling the roll() method from the object assigned to the tire field
+        tire.roll();
+    }
+}
