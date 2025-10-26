02be1d027c4bb02f8350ea63536f9053b656b89a
kimuni01 <minhowin1@naver.com>
2025-10-26 16:59:09 +0900

    Add files via upload

---
+++ ch07/sec08/exam02/Driver.java
@@ -0,0 +1,8 @@
+package ch07.sec08.exam02;
+
+public class Driver {
+    // declaring method(has a parameter of a class type)
+    public void drive(Vehicle vehicle) {
+        vehicle.run();
+    }
+}
