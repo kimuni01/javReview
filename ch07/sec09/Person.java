c1d8ccf713066c71a733b85a470d4240d9f570e5
kimuni01 <minhowin1@naver.com>
2025-10-27 19:36:03 +0900

    Add files via upload

---
+++ ch07/sec09/Person.java
@@ -0,0 +1,16 @@
+package ch07.sec09;
+
+public class Person {
+    // declaring field
+    public String name;
+
+    // declaring constructor
+    public Person(String name) {
+        this.name = name;
+    }
+
+    // declaring method
+    public void walk() {
+        System.out.println("걷습니다.");
+    }
+}
