770bbb5fb55ec0efe4f7c892ca2be97a25528adc
kimuni01 <minhowin1@naver.com>
2025-11-01 16:23:02 +0900

    Add files via upload

---
+++ ch08/sec03/RemoteControl.java
@@ -0,0 +1,6 @@
+package ch08.sec03;
+
+public interface RemoteControl {
+    int MAX_VOLUME = 10; // at all times, public static final automatically addded
+    int MIN_VOLUME = 0; // during compile times
+}
