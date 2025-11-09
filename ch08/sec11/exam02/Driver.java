2cefb6b95aa5bd833fdd5f0014dbbbc7ecaa39d0
kimuni01 <minhowin1@naver.com>
2025-11-09 10:22:30 +0900

    Add files via upload

---
+++ ch08/sec11/exam02/Driver.java
@@ -0,0 +1,7 @@
+package ch08.sec11.exam02;
+
+public class Driver {
+    void drive(Vehicle vehicle) { // declaring parameter as the interface type
+        vehicle.run(); // calling the interface method
+    }
+}
