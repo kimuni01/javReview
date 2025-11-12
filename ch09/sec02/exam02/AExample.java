4939ec886b4e70cb6ecd4ab7159fbd643ee94689
kimuni01 <minhowin1@naver.com>
2025-11-12 21:23:35 +0900

    Add files via upload

---
+++ ch09/sec02/exam02/AExample.java
@@ -0,0 +1,11 @@
+package ch09.sec02.exam02;
+
+public class AExample {
+    public static void main(String[] args) {
+        // creating object A
+        A a = new A();
+
+        // calling the A instance method
+        a.useB();
+    }
+}
