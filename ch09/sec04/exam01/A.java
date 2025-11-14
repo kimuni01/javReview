4233447c7a024ce30c92996f26e424beef5d8ed4
kimuni01 <minhowin1@naver.com>
2025-11-14 21:34:15 +0900

    Add files via upload

---
+++ ch09/sec04/exam01/A.java
@@ -0,0 +1,21 @@
+package ch09.sec04.exam01;
+
+public class A {
+    // constructor
+    A() {
+        // declaring local class
+        class B {}
+
+        // creating local object
+        B b = new B();
+    }
+
+    // method
+    void method() {
+        // declaring local class
+        class B {}
+
+        // creating local object
+        B b = new B();
+    }
+}
