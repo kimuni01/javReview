d77ac2b293b7f870ad757a0d78778227be0e77be
kimuni01 <minhowin1@naver.com>
2025-11-16 17:41:12 +0900

    Add files via upload

---
+++ ch09/sec06/exam01/Button.java
@@ -0,0 +1,9 @@
+package ch09.sec06.exam01;
+
+public class Button {
+    // static nested interface
+    public static interface ClickListener {
+        // abstract method
+        void onClick();
+    }
+}
