373cb8d281d8393848d39a0f589c7bad023dcc59
kimuni01 <minhowin1@naver.com>
2025-12-14 23:30:02 +0900

    Add files via upload

---
+++ ch02/test001/ch02test001.java
@@ -0,0 +1,23 @@
+package ch02.test001;
+
+public class ch02test001 {
+    public static void main(String[] args) {
+        int intValue = 10;
+        char charValue = 'A';
+        double doubleValue = 5.7;
+        String strValue = "A";
+
+        double var1 = (double) intValue;
+        byte var2 = (byte) intValue;
+        int var3 = (int) doubleValue;
+        // char var4 = (char) strValue;
+
+        // char var3 = '';
+
+        // int class = 0;
+        // int 6hour = 0;
+        int $value = 0;
+        int _age = 0;
+        // int #name = 0;
+    }
+}
